075c27ad23682b25b6a557c1576ad31995b05830
Ryan Walmsley <ryanteck@gmail.com>
2018-08-07 12:56:04 +0100

    Added new version of the pacakge ready for initial apt testing

---
+++ iotLoRaGateway-0.0-4/opt/iot-lora-gateway/global_confs/start-pin22.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "22" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio22/direction
+echo "1" > /sys/class/gpio/gpio22/value
+sleep 1
+echo "0" > /sys/class/gpio/gpio22/value
+cd /opt/iot-lora-gateway/
+./mp_pkt_fwd
+echo "22" >/sys/class/gpio/unexport
