55ba4dc281a818d497c0bccab85f3cf8b8c338a7
Ton van Overbeek <tvoverbeek@gmail.com>
2018-11-25 20:13:24 +0100

    Fixes for iot-lora-configure
    Selecting cancel in iot-lora-configure dialogs now exits the program.
    Improved user experience by prefilling fields when possible.
    Fix iot-lora-gateway-reset.sh to avoid error/warning when gpio22 is already exported.

--- template_configs/iot-lora-gateway-reset.sh
+++ template_configs/iot-lora-gateway-reset.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "22" > /sys/class/gpio/export
+if [ ! -d "/sys/class/gpio/gpio22" ]; then
+    echo "22" > /sys/class/gpio/export
+fi
 echo "out" > /sys/class/gpio/gpio22/direction
 echo "1" > /sys/class/gpio/gpio22/value
 sleep 1
